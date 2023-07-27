55e0c5ba17063aba5bda79b1ac92b666885f08d9
Kalle Lodin <kalo22@student.bth.se>
2023-07-27 04:32:37 +0200

    Implemented /library api on the /api page

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\LibraryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,5 +39,51 @@ class ApiController extends AbstractController
             'date' => $timestamp->format('Y-m-d'),
             'timestamp' => $timestamp->format('H:i:s'),
         ]);
+    }
+
+    /**
+     * @Route("/api/library/books", name="api_books")
+     */
+    public function getBooks(LibraryRepository $libraryRepository): JsonResponse
+    {
+        $books = $libraryRepository->findAll();
+        $booksArray = [];
+        foreach($books as $book){
+            $booksArray[] = [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'author' => $book->getAuthor(),
+                'isbn' => $book->getIsbn(),
+            ];
+        }
+
+        $response = $this->json($booksArray);
+        $response->setEncodingOptions($response->getEncodingOptions() | JSON_PRETTY_PRINT);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/library/book/{isbn}", name="api_book")
+     */
+    public function getBookByIsbn($isbn, LibraryRepository $libraryRepository): JsonResponse
+    {
+        $book = $libraryRepository->findOneBy(['isbn' => $isbn]);
+
+        if (!$book) {
+            return new JsonResponse(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $bookArray = [
+            'id' => $book->getId(),
+            'title' => $book->getTitle(),
+            'author' => $book->getAuthor(),
+            'isbn' => $book->getIsbn(),
+        ];
+
+        $response = $this->json($bookArray);
+        $response->setEncodingOptions($response->getEncodingOptions() | JSON_PRETTY_PRINT);
+
+        return $response;
     }
 }
