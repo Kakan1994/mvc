bba0eaa75e0c34d4f658ea01f3159671f27622c8
Kalle Lodin <kalo22@student.bth.se>
2023-07-29 20:47:23 +0200

    Added tests for GameController

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use App\Game\Game;
+
+
+
+class HomeControllerTest extends WebTestCase
+{
+    
+}
