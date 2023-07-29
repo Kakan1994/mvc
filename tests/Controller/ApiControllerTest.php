0cf294d3b679ef7daa2309de2fbf702d4358a739
Kalle Lodin <kalo22@student.bth.se>
2023-07-29 08:40:49 +0200

    Testing unittests with scrutinizer

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -4,34 +4,23 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use App\Entity\Library;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiControllerTest extends WebTestCase
 {
-    public function testIndex(): void
-    {
-        $kernel = self::bootKernel();
-        $client = $kernel->getContainer()->get('test.client');
-
-        /** @var KernelBrowser $client */
-        $client->request('GET', '/api');
-
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-    }
+    // public function testIndex(): void
+    // {
+    //     $client = static::createClient();
+    //     $client->request('GET', '/api');
+    //     $this->assertResponseIsSuccessful();
+    // }
 
     public function testQuote(): void
     {
-        $kernel = self::bootKernel();
-        $client = $kernel->getContainer()->get('test.client');
-
-        /** @var KernelBrowser $client */
+        $client = static::createClient();
         $client->request('GET', '/api/quote');
-
-        $response = $client->getResponse();
-
-        $this->assertSame(200, $response->getStatusCode());
-        $this->assertJson($response->getContent());
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
     }
 }
